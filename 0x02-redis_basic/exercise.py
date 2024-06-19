4467ee1503b319f0abbb30f64a2ace13ebd52358
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-19 20:05:37 +0300

    Add solution for task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Module containing class Cache"""
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -17,3 +17,17 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """Get data from redis"""
+        if fn is None:
+            return self._redis.get(key)
+        return fn(self._redis.get(key))
+    
+    def get_str(self, key: str) -> str:
+        """Get data from redis as a string"""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """Get data from redis as an integer"""
+        return self.get(key, int)
